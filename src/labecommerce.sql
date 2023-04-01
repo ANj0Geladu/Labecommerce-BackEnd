757b60a659054078993d7c8be595dcbbfbdfa38f
victor <jv.paixao.762.g@gmail.com>
2023-03-31 23:50:27 -0300

    feito

--- src/labecommerce.sql
+++ src/labecommerce.sql
@@ -28,3 +28,42 @@ CREATE TABLE products (
     price REAL NOT NULL,
     category TEXT NOT NULL
 );
+
+
+--------aprofundamento SQL
+
+SELECT * FROM usuarioxs;
+
+SELECT * FROM products;
+
+INSERT INTO usuarioxs
+ (id, email, password) 
+ VALUES 
+ ('usuariox4', 'usuariox4@example.com', 'password4');
+
+
+
+
+INSERT INTO products 
+(id, name, price, category) 
+VALUES ('product1', 'Monitor LG 24"', 699.99, 'Eletrônicos');
+
+
+SELECT * FROM products WHERE id = 'product1';
+DELETE FROM usuarioxs WHERE id = 'usuariox4';
+DELETE FROM products WHERE id = 'product1';
+
+
+UPDATE usuarioxs SET email = 'new_email@example.com' WHERE id = 'usuariox1';
+
+
+UPDATE products SET price = 799.99 WHERE id = 'product2';
+
+
+SELECT * FROM usuarioxs ORDER BY email ASC;
+SELECT * FROM products ORDER BY price ASC LIMIT 20 OFFSET 0;
+SELECT * FROM products WHERE price BETWEEN 100.00 AND 300.00 ORDER BY price ASC;
+
+
+
+
